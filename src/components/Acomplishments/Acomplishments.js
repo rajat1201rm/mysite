3e65db95016cd3d80c7ab6ebbc34fb0b65182838
rajat1201rm <66858773+rajat1201rm@users.noreply.github.com>
2021-06-19 02:11:32 +0530

    Update Acomplishments.js

--- src/components/Acomplishments/Acomplishments.js
+++ src/components/Acomplishments/Acomplishments.js
@@ -6,7 +6,7 @@ import { Box, Boxes, BoxNum, BoxText } from './AcomplishmentsStyles';
 const data = [
   { number: 100, text: 'Team Members @ SME VIT '},
   { number: 1, text: 'Scholar Memento', },
-  { number: 3, text: 'Scholar Badges', },
+  { number: 4, text: 'Scholar Badges', },
   { number: 2, text: 'Gold Medal at States Kickboxing ', }
  
 ];
