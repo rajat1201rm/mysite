6d89518b5d6633abdc2648aeff982b79f730492c
rajat1201rm <66858773+rajat1201rm@users.noreply.github.com>
2021-06-19 19:07:22 +0530

    Update ProfilePic.js

--- src/components/Projects/ProfilePic.js
+++ src/components/Projects/ProfilePic.js
@@ -3,15 +3,14 @@ import React from 'react';
 import { Image } from 'next-images'
 import dp from '../../../public/images/4.jpg'
 
-const myLoader = ({ src, width, quality }) => {
-    return `https://example.com/${src}?w=${width}&q=${quality || 75}`
-  }
 const ProfilePic = () => {
     return (
      
    <div>
      HI
-     <Image src={dp} loader={myLoader}/>
+     <Image src={dp}
+alt='error'
+/>
    </div>
             
     )
